77c8191f185a4643d84792affa669e57504fe7be
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-03-28 15:46:28 +0000

    Added comment to server

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ import bodyParser from 'body-parser';
 
 const articlesInfo = {
     'learn-react': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'learn-node': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'my-thoughts-on-resumes': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     }
 }
 
@@ -20,8 +23,15 @@ app.use(bodyParser.json());
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articlesInfo[articleName].upvotes++;
-    res.status(200).send(`${articleName} now has $a{rticlesInfo[articleName].upvotes} upvotes`)
-})
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+});
+
+app.post('api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    articlesInfo[articleName].comments.push({ username, text });
+    res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(8000, () => console.log('listening on port 8000'));
 
