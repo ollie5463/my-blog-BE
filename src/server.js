f8ede4256e77e5a90906938f7467d5d3003bd381
oliver.bannister <oliver.bannister@roxorgaming.co.uk>
2020-03-28 15:28:29 +0000

    init

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0
+    },
+    'learn-node': {
+        upvotes: 0
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0
+    }
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes++;
+    res.status(200).send(`${articleName} now has $a{rticlesInfo[articleName].upvotes} upvotes`)
+})
+
+app.listen(8000, () => console.log('listening on port 8000'));
+
